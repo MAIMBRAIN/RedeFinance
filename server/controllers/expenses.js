a5138d1a154747d27642297e2fa73696d8541f4c
Avery Stahl <averystahl@hotmail.com>
2020-03-04 12:06:33 -0500

    created expense model and controller

---
+++ server/controllers/expenses.js
@@ -0,0 +1,71 @@
+const Expense = require('../models/expense.js');
+
+module.exports = {
+    // create new expense
+    create: async (req, res) => {
+        try {
+            const expense = Expense.create(req.body);
+            res.json({
+                success: true,
+                message: "New expense created",
+                expense
+            });
+        } catch (err) {
+            res.json({
+                success: false,
+                code: err.code
+            });
+        }
+    },
+
+    // show all expenses
+    index: async (req, res) => {
+        try {
+            const expense = Expense.find({});
+            res.json(expense);
+        } catch (err) {
+            alert(err);
+        }
+    },
+
+    // show variable-rate expenses
+    variable: async (req, res) => {
+        try {
+            const expense = Expense.find({
+                type: "variable-rate"
+            });
+            res.json(expense);
+        } catch (err) {
+            alert(err);
+        }
+    },
+
+    // show flat-rate expenses
+    flat: async (req, res) => {
+        try {
+            const expense = Expense.find({
+                type: "flat-rate"
+            });
+            res.json(expense);
+        } catch (err) {
+            alert(err);
+        }
+    },
+
+    // delete an expense
+    destroy: async (req, res) => {
+        try {
+            const expense = await Expense.findByIdAndRemove(req.params.id);
+            res.json({
+                success: true,
+                message: "Expense deleted",
+                expense
+            });
+        } catch (err) {
+            res.json({
+                success: false,
+                code: err.code
+            });
+        }
+    }
+};
