6fd78350e307be4806e32fe1cf23ac6687c88264
Avery Stahl <averystahl@hotmail.com>
2020-03-11 15:29:52 -0400

    Changed Navbar links, styled login / signup, styled home a little

--- client/src/components/Header/NavBar.js
+++ client/src/components/Header/NavBar.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {Typography} from '@material-ui/core'
 import './NavBar.css';
 
 const NavBar = (props) => {
     return (
         <div className = "header">
             {/* Logo */}
-            <Link className = "nav-title" to="/">
-                <img className = "nav-logo" src={ "/logo192.png" } alt="React logo" />
-            </Link>
+            <div className = "nav-title">
+                <Typography variant={'h4'}>
+                    RedeFinance
+                </Typography>
+            </div>
 
             {/* Page Links */}
             <div className = "nav-items">
-                <Link className = "nav-link" to='/Home'>Home</Link>
-                <a className = "nav-link" target='_blank' rel="noopener noreferrer" href="https://reactjs.org/docs/getting-started.html">
-                    React Docs
-                </a>
                 {props.currentUser ?
                     (
                         <span>
-                            <Link className ="nav-link" to='/dashboard'>Profile</Link>
-                            <Link className ="nav-link" to='/logout'>Log Out</Link>
+                            <Typography variant={'h6'}>
+                                <Link className ="nav-link" to='/dashboard'>Dashboard</Link>
+                                <Link className ="nav-link" to='/logout'>Log Out</Link>
+                            </Typography>
                         </span>
                     ) :
                     (
                     <span>
-                        <Link className ="nav-link" to="/login">Log In</Link>
-                        <Link className ="nav-link" to="/signup">Sign Up</Link>
+                        <Typography variant={'h6'}>
+                            <Link className = "nav-link" to='/Home'>Home</Link>
+                            <Link className ="nav-link" to="/login">Log In</Link>
+                            <Link className ="nav-link" to="/signup">Sign Up</Link>
+                        </Typography>
                     </span>
                     )
                 }
             </div>
-
         </div>
     )
 };
